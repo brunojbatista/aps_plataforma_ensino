71c57e1d3853a520e7fa78f550bc3ac27e07ecd3
Bruno Batista <brunojbatista@hotmail.com>
2021-11-07 18:59:32 -0300

    Criação da Fachada

---
+++ Entrega 2/codigo_js/src/Controllers/Fachada.js
@@ -0,0 +1,45 @@
+class Fachada {
+
+    static cadastrarUsuario(
+        cpf,
+        nome,
+        login,
+        senha,
+        email,
+        telefone
+    ) {
+
+    }
+
+    static fazerLogin(
+        login,
+        senha
+    ) {
+
+    }
+
+    static checarAutenticacao(
+        usuario_id
+    ) {
+
+    }
+
+    static comprarCurso(
+        curso_id,
+        usuario_id
+    ) {
+
+    }
+
+    static cadastrarCurso(
+        usuario_id,
+        nome,
+        descricao,
+        preco
+    ) {
+
+    }
+
+}
+
+module.exports = Fachada;

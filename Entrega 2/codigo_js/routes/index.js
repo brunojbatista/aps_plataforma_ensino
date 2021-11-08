a24d1b643dd90139b8580350a9c93e6b745a4cfa
rodrigo <rfop@cin.ufpe.br>
2021-11-07 21:18:47 -0300

    views finalizada

--- Entrega 2/codigo_js/routes/index.js
+++ Entrega 2/codigo_js/routes/index.js
@@ -3,11 +3,46 @@ const GlobalUtils = require("../src/Utils/Global");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/TelaLogin', function(req, res, next) {
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('TelaLogin'));
+});
+
+router.get('/TelaInicial', function(req, res, next) {
 
   res.set('Content-Type', 'text/html');
 
   res.sendFile(GlobalUtils.getHTMLFile('TelaInicial'));
+});
+
+router.get('/TelaCurso', function(req, res, next) {
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('TelaCurso'));
+});
+
+router.get('/CadastroAluno', function(req, res, next) {
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('CadastroAluno'));
+});
+
+router.get('/CadastroCurso', function(req, res, next) {
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('CadastroCurso'));
+});
+
+router.get('/TelaApresentacao', function(req, res, next) {
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('TelaApresentacao'));
 });
 
 module.exports = router;

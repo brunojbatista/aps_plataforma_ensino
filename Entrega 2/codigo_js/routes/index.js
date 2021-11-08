f8034efdc98b8a73a8c89264131cc49411970790
Bruno Batista <brunojbatista@hotmail.com>
2021-11-07 22:57:17 -0300

    Atualizacao

--- Entrega 2/codigo_js/routes/index.js
+++ Entrega 2/codigo_js/routes/index.js
@@ -3,7 +3,7 @@ const GlobalUtils = require("../src/Utils/Global");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/TelaLogin', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
   res.set('Content-Type', 'text/html');
 
@@ -43,6 +43,38 @@ router.get('/TelaApresentacao', function(req, res, next) {
   res.set('Content-Type', 'text/html');
 
   res.sendFile(GlobalUtils.getHTMLFile('TelaApresentacao'));
+});
+
+const Sessao     = require('../src/Models/Sessao');
+
+router.get('/teste', async (req, res) => {
+
+  let sessao = new Sessao(req, res);
+
+  // console.log(sessao);
+
+  console.log(sessao.isLogged());
+
+  // console.log(sessao.setLogged());
+
+  // const sequelize = new Sequelize('plataforma', 'root', 'root', {
+  //     host: 'localhost',
+  //     dialect: 'mysql'
+  // });
+
+  // console.log(sequelize);
+
+  // try {
+  //     await sequelize.authenticate();
+  //     console.log('Connection has been established successfully.');
+  //   } catch (error) {
+  //     console.error('Unable to connect to the database:', error);
+  //   }
+
+  res.set('Content-Type', 'text/html');
+
+  res.sendFile(GlobalUtils.getHTMLFile('TelaInicial'));
+
 });
 
 module.exports = router;

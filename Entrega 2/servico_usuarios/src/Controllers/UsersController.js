0f824e7457f0a4a4e19832d3da4d518a1e212a25
Victor Miranda <vmmelo1@gmail.com>
2021-12-15 12:56:13 -0300

    removendo lixo

--- Entrega 2/servico_usuarios/src/Controllers/UsersController.js
+++ Entrega 2/servico_usuarios/src/Controllers/UsersController.js
@@ -2,7 +2,7 @@ const Fachada           = require('../Fachada');
 const Formidable    = require('formidable');
 
 exports.cadastrarUsuario = (req, res, next) => {
-    
+
     // var fabrica = new FabricaRepositorioBDR();
 
     // var usuario = fabrica.criarRepositorioUsuario();
@@ -12,7 +12,7 @@ exports.cadastrarUsuario = (req, res, next) => {
     var form = new Formidable.IncomingForm();
 
     form.parse(
-        req, 
+        req,
         async function (err, fields, files) {
 
             try {
@@ -31,57 +31,9 @@ exports.cadastrarUsuario = (req, res, next) => {
             }
 
         }
-    );   
+    );
 
-    // var form = new Formidable.IncomingForm();
-
-    // form.parse(req, async function (err, fields, files) {
-    
-    //     // console.log(fields);
-
-    //     try {
-    //         await Fachada.cadastrarUsuario(
-    //             fields
-    //         );
-    //         res.status(200).json({
-    //             'code': 200,
-    //             'msg': 'Usuário cadastrado com sucesso!'
-    //         });
-    //     } catch (e) {
-    //         res.status(400).json({
-    //             'code': 400,
-    //             'msg': e
-    //         });
-    //     }
-
-    // });
-    
-
-    
-
-
-
-    // try {
-    //     Fachada.cadastrarUsuario(
-    //         req.body.cpf,
-    //         req.body.nome,
-    //         req.body.login,
-    //         req.body.senha,
-    //         req.body.email,
-    //         req.body.telefone
-    //     );
-    //     res.status(200).json({
-    //         'code': 200,
-    //         'msg': 'Usuário cadastrado com sucesso!'
-    //     });
-    // } catch (e) {
-    //     res.status(400).json({
-    //         'code': 400,
-    //         'msg': e
-    //     });
-    // }
-    // res.status(200).send(`Requisição recebida com sucesso!`);
-};
+}
 
 exports.autenticar = (req, res, next) => {
 
--- Entrega 2/servico_usuarios/src/Models/Repositories/FabricaRepositorioBDR.js
+++ Entrega 2/servico_usuarios/src/Models/Repositories/FabricaRepositorioBDR.js
@@ -1,12 +1,5 @@
 const FabricaRepositorioAbstrata    = require('./FabricaRepositorioAbstrata');
 const RepositorioUsuarioBDR         = require('./BDR/RepositorioUsuarioBDR');
-const RepositorioCursoBDR           = require('./BDR/RepositorioCursoBDR');
-const RepositorioCartaoBDR          = require('./BDR/RepositorioCartaoBDR');
-const RepositorioMatriculaBDR       = require('./BDR/RepositorioMatriculaBDR');
-const RepositorioTransacaoBDR       = require('./BDR/RepositorioTransacaoBDR');
-// const CursoBDR          = require('./CursoBDR');
-// const CartaoBDR         = require('./CartaoBDR');
-// const MatriculaBDR      = require('./MatriculaBDR');
 
 class FabricaRepositorioBDR extends FabricaRepositorioAbstrata {
 
@@ -15,24 +8,6 @@ class FabricaRepositorioBDR extends FabricaRepositorioAbstrata {
     criarRepositorioUsuario() {
         return new RepositorioUsuarioBDR();
     }
-
-    criarRepositorioCurso() {
-        return new RepositorioCursoBDR();
-        // console.log("aaaaaaaaa")
-    }
-
-    criarRepositorioCartao() {
-        return new RepositorioCartaoBDR();
-    }
-
-    criarRepositorioMatricula() {
-        return new RepositorioMatriculaBDR();
-    }
-
-    criarRepositorioTransacao() {
-        return new RepositorioTransacaoBDR();
-    }
-
 }
 
 module.exports = FabricaRepositorioBDR;
